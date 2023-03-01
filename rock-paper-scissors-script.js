89799196686382937f709314f0f80efd36db0ea6
Jeff Perrin <jeffreyperrin10@gmail.com>
2023-03-01 16:53:28 -0500

    Add playRound function

--- rock-paper-scissors-script.js
+++ rock-paper-scissors-script.js
@@ -2,12 +2,31 @@
 function getComputerChoice() {
     let randomValue = Math.random();
     if (randomValue < .33) {
-        return 'Rock';
+        return 'rock';
     } else if (randomValue < .66) {
-        return 'Paper';
+        return 'paper';
     } else {
-        return 'Scissors';
+        return 'scissors';
     }
 }
 
-console.log(getComputerChoice());
+//Play a single round of Rock-Paper-Scissors
+function playRound(playerSelection, computerSelection) {
+    //Get playerSelection
+    let getPlayerSelection = prompt('What is your choice?', playerSelection).toLowerCase();
+    console.log(getPlayerSelection);
+    //Get computerSelection (use getComputerChoice())
+    let getComputerSelection = getComputerChoice();
+    console.log(getComputerSelection);
+    //Compare selections to determine winner or tie then return game result declaration
+    if (getPlayerSelection === getComputerSelection) {
+        return "It's a tie!";
+    } else if ((getPlayerSelection === 'rock' && getComputerSelection === 'scissors') || (getPlayerSelection === 'paper' && getComputerSelection === 'rock') || (getPlayerSelection === 'scissors' && getComputerSelection === 'paper')) {
+        return `You win! ${getPlayerSelection} beats ${getComputerSelection}!`;
+    } else {
+        return `You lose! ${getComputerSelection} beats ${getPlayerSelection}`
+    }
+}
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
