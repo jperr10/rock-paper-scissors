46ff54cd7b1eff0450f7429879da5ad75aed5419
Jeff Perrin <jeffreyperrin10@gmail.com>
2023-04-02 13:36:49 -0400

    Add running score

--- rock-paper-scissors-script.js
+++ rock-paper-scissors-script.js
@@ -12,13 +12,18 @@ function getComputerChoice() {
         return computerChoice = 'scissors';
     }
 }
-
+let pScore = 0;
+const playerScore = document.querySelector('.playerScore');
+playerScore.textContent = `Player: ${pScore}`;
+let cScore = 0;
+const computerScore = document.querySelector('.computerScore');
+computerScore.textContent = `Computer: ${cScore}`;
 
 // Add event listener to buttons to play the game
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
-    let playerScore = 0;
-    let computerScore = 0;
+    //let playerScore = 0;
+    //let computerScore = 0;
     button.addEventListener('click', () => {
         const playerSelection = button.id;
         console.log(playerSelection);
@@ -29,11 +34,11 @@ buttons.forEach((button) => {
         if (playerSelection === computerSelection) {
             roundResult.textContent = "This round is a tie";
         } else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
-            roundResult.textContent = "You won this round!";
-            playerScore = ++playerScore;
+            roundResult.textContent = `You won this round! ${playerSelection} beats ${computerSelection}`;
+            playerScore.textContent =`Player: ${++pScore}`;
         } else if ((playerSelection === "rock" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "rock")) {
-            roundResult.textContent = "You lost this round :/";
-            computerScore = ++computerScore;
+            roundResult.textContent = `You lost this round. ${computerSelection} beats ${playerSelection}`;
+            computerScore.textContent = `Computer: ${++cScore}`;
         } else {
             roundResult.textContent = "An error occurred";
         }
